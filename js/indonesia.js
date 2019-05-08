4492b8eaf23c9f98587b54ffab1b8e74f5231559
aldrichvalentino <aldrich.vh97@gmail.com>
2019-05-08 16:00:53 +0700

    Added Indonesia APM distribution map

--- js/indonesia.js
+++ js/indonesia.js
@@ -1,135 +1,149 @@
-/* Global variables */
-var width = 960,
-  height = 500,
-  centered;
-var currentYear = 0;
-var projection = d3.geoEquirectangular()
-  .scale(1050)
-  .rotate([-120, 0])
-  .translate([width / 2, height / 2]);
-var path = d3.geoPath()
-  .projection(projection);
+var margin = { top: 20, right: 20, bottom: 20, left: 20 };
+width = 1000 - margin.left - margin.right,
+  height = 512 - margin.top - margin.bottom,
+  formatPercent = d3.format(".1%");
 
-/* Select elements */
-var svg = d3.select("#map")
-  .attr("width", width)
-  .attr("height", height);
-svg.append("rect")
-  .attr("class", "background")
-  .attr("width", width)
-  .attr("height", height)
-  .on("click", mouseover);
-var g = svg.append("g");
+var svg = d3.select("#map").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 tooltip = d3.select("body").append("div")
   .attr("class", "tooltip")
   .style("opacity", 0);
-var yearSlider = d3.select("#year-slider");
-var sideInfo = d3.select("#side-info").style("color", "white");
-var yearLabel = d3.select("#current-year");
 
-var color = d3.scaleThreshold()
-  .domain([87.5, 90, 92.5, 95, 97.5])
-  .range(["#CCCCFF", "#66CCCC", "#6699CC", "#3366CC", "#333366"]);
+var promises = [];
+promises.push(d3.csv("data/indonesia.csv"));
+promises.push(d3.json("data/indonesia.json"));
 
-/* Import JSON Map data */
-d3.json("data/indonesia.json").then(function (us) {
-  var stateProvinces = topojson.feature(us, us.objects.states_provinces).features;
+Promise.all(promises)
+  .then(values => {
+    ready(null, values[0], values[1]);
+  })
+  .catch(error => console.error(error));
 
-  /* Import csv APM data */
-  d3.csv("data/indonesia.csv").then(data => {
-    var dataByProvince = d3.nest()
-      .key(function (d) { return d.province; })
-      .map(data);
+var legendText = ["", "87.5", "90", "92.5", "95", "97.5"];
+var legendColors = ["#CCCCFF", "#66CCCC", "#6699CC", "#3366CC", "#333366"];
 
-    currentYear = data[0].year;
 
-    stateProvinces.forEach(singleProvince => {
-      singleProvince.yearlyAPM = dataByProvince["$" + singleProvince.properties.name]
+function ready(error, data, us) {
+  // console.log(us.objects);
+  var counties = topojson.feature(us, us.objects.states_provinces);
+
+  // data.forEach(function (d) {
+  //   d.year = +d.year;
+  //   d.fips = +d.fips;
+  //   d.rate = +d.rate;
+  // });
+
+  var dataByCountyByYear = d3.nest()
+    .key(function (d) { return d.province; })
+    .key(function (d) { return d.year; })
+    .map(data);
+
+  // console.log(dataByCountyByYear);
+  // console.log(counties.features);
+  counties.features.forEach(function (county) {
+    // console.log(county);
+    // console.log("$" + county.properties.name);
+    county.properties.years = dataByCountyByYear["$" + county.properties.name];
+  });
+  // console.log(counties.features);
+
+  var color = d3.scaleThreshold()
+    .domain([87.5, 90, 92.5, 95, 97.5])
+    .range(["#CCCCFF", "#66CCCC", "#6699CC", "#3366CC", "#333366"]);
+
+  var projection = d3.geoEquirectangular()
+    .scale(1050)
+    .rotate([-120, 0])
+    .translate([width / 2, height / 2]);
+
+  var path = d3.geoPath()
+    .projection(projection);
+
+  var countyShapes = svg.selectAll(".county")
+    .data(counties.features)
+    .enter()
+    .append("path")
+    .attr("class", "county")
+    .attr("d", path);
+
+  countyShapes
+    .on("mouseover", function (d) {
+      tooltip.transition()
+        .duration(250)
+        .style("opacity", 1);
+      tooltip.html(
+        "<p><strong>" + d.properties.years["$1996"][0].province + ", " + d.properties.years["$1996"][0].province + "</strong></p>" +
+        "<table><tbody><tr><td class='wide'>APM rate in 1996:</td><td>" + formatPercent((d.properties.years["$1996"][0].APM) / 100) + "</td></tr>" +
+        "<tr><td>APM rate in 1998:</td><td>" + formatPercent((d.properties.years["$1998"][0].APM) / 100) + "</td></tr>" +
+        "<tr><td>Change:</td><td>" + formatPercent((d.properties.years["$1998"][0].APM - d.properties.years["$1996"][0].APM) / 100) + "</td></tr></tbody></table>"
+      )
+        .style("left", (d3.event.pageX + 15) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+    })
+    .on("mouseout", function (d) {
+      tooltip.transition()
+        .duration(250)
+        .style("opacity", 0);
     });
 
-    g.append("g")
-      .attr("id", "subunits")
-      .selectAll("path")
-      .data(stateProvinces)
-      .enter().append("path")
-      .attr("d", path)
-      .on("mouseover", mouseover)
-      .on("mouseout", mouseout)
-      .style("fill", function (d) {
-        if (!!d.yearlyAPM) {
-          return color(d.yearlyAPM[0].APM);
-        }
-      });
-    g.append("path")
-      .datum(topojson.mesh(us, us.objects.states_provinces, function (a, b) { return a !== b; }))
-      .attr("id", "state-borders")
-      .attr("d", path);
+  // console.log(us.objects.states_provinces);
+  svg.append("path")
+    .datum(topojson.feature(us, us.objects.states_provinces, function (a, b) { return a !== b; }))
+    .attr("class", "states")
+    .attr("d", path);
 
-    yearLabel.text(data[0].year);
-    yearSlider
-      .attr("min", data[0].year)
-      .attr("max", data[data.length - 1].year)
-      .on("input", function () {
-        currentYear = yearSlider.property("value"); // get the current year
-        yearLabel.text(currentYear);
-        var dataSelection = svg.selectAll("g").data(stateProvinces);
-        // TODO: change data
-      });
-    // sideInfo
-    //   .data(data)
-    //   .enter().append("li")
-    //   .text(function (d) {
-    //     return d.value
-    //   });
-    // TODO: change side info data
-  }).catch(error => console.error(error));
-}).catch(error => console.error(error));
+  var legend = svg.append("g")
+    .attr("id", "legend");
 
-function regionInfo(region) {
-  return region.properties.name ? region.properties.name.toUpperCase() : region.properties.name;
+  var legenditem = legend.selectAll(".legenditem")
+    .data(d3.range(6))
+    .enter()
+    .append("g")
+    .attr("class", "legenditem")
+    .attr("transform", function (d, i) { return "translate(" + i * 31 + ",0)"; });
+
+  legenditem.append("rect")
+    .attr("x", width - 240)
+    .attr("y", -7)
+    .attr("width", 30)
+    .attr("height", 6)
+    .attr("class", "rect")
+    .style("fill", function (d, i) { return legendColors[i]; });
+
+  legenditem.append("text")
+    .attr("x", width - 240)
+    .attr("y", -10)
+    .style("text-anchor", "middle")
+    .text(function (d, i) { return legendText[i]; });
+
+  function update(year) {
+    slider.property("value", year);
+    d3.select(".year").text(year);
+    countyShapes.style("fill", function (d) {
+      const years = d.properties.years;
+      if (years) {
+        return color(d.properties.years["$" + year][0].APM)
+      }
+    });
+  }
+
+  var slider = d3.select(".slider")
+    .append("input")
+    .attr("type", "range")
+    .attr("min", 1996)
+    .attr("max", 1998)
+    .attr("step", 1)
+    .on("input", function () {
+      var year = this.value;
+      update(year);
+    });
+
+  update(1996);
+
 }
 
-function mouseover(d) {
-  if (d) {
-    document.getElementById('info').innerHTML = regionInfo(d);
-  } else {
-    document.getElementById('info').innerHTML = "INDONESIA";
-  }
-  if (d && centered !== d) {
-    var centroid = path.centroid(d);
-    x = centroid[0];
-    y = centroid[1];
-    k = 4;
-    centered = d;
-  } else {
-    x = width / 2;
-    y = height / 2;
-    k = 1;
-    centered = null;
-  }
-  g.selectAll("path")
-    .classed("active", centered && function (d) { return d === centered; });
-
-  tooltip.transition()
-    .duration(250)
-    .style("opacity", 1);
-
-  try {
-    const provinceData = d.yearlyAPM.filter(singleData => singleData.year == currentYear)[0];
-    tooltip.html("test")
-    tooltip.html(
-      "<p><strong>" + provinceData.province + "</strong></p>" +
-      "<p>APM rate in " + currentYear + ": " + provinceData.APM + "%</p>"
-    )
-      .style("left", (d3.event.pageX + 15) + "px")
-      .style("top", (d3.event.pageY - 28) + "px");
-  } catch (error) {
-    // do nothing
-  }
-}
-
-function mouseout(d) {
-  tooltip.transition()
-    .duration(250)
-    .style("opacity", 0);
-}
+d3.select(self.frameElement).style("height", "685px");
