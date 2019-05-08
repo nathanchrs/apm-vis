d7e6455ec7f74ce9972b236363d718323378f602
aldrichvalentino <aldrich.vh97@gmail.com>
2019-05-08 23:50:01 +0700

    Added distribution info

--- js/indonesia.js
+++ js/indonesia.js
@@ -1,15 +1,36 @@
-var margin = { top: 20, right: 20, bottom: 20, left: 20 };
-width = 1000 - margin.left - margin.right,
-  height = 512 - margin.top - margin.bottom,
+var mapMargin = { top: 20, right: 20, bottom: 20, left: 20 };
+var width = 1000 - mapMargin.left - mapMargin.right,
+  height = 512 - mapMargin.top - mapMargin.bottom,
   formatPercent = d3.format(".1%");
 
-var svg = d3.select("#map").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var distributionMargins = { top: 20, right: 20, bottom: 30, left: 40 },
+  widths = 90 - distributionMargins.left - distributionMargins.right,
+  heights = 600 - distributionMargins.top - distributionMargins.bottom;
 
-tooltip = d3.select("body").append("div")
+var yValue = function (d) { return d.APM },
+  yScale = d3.scaleLinear().range([height, 0])
+    .domain([70, 100]),
+  yMap = function (d) { return yScale(yValue(d)) },
+  yAxis = d3.axisLeft(yScale);
+
+var mapSvg = d3.select("#map").append("svg")
+  .attr("width", width + mapMargin.left + mapMargin.right)
+  .attr("height", height + mapMargin.top + mapMargin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + mapMargin.left + "," + mapMargin.top + ")");
+
+var distributionSvg = d3.select("#distribution").append("svg")
+  .attr("width", widths + distributionMargins.left + distributionMargins.right)
+  .attr("height", heights + distributionMargins.top + distributionMargins.bottom)
+  .style("margin-left", distributionMargins.left)
+  .append("g")
+  .attr("transform", "translate(" + distributionMargins.left + "," + distributionMargins.top + ")");
+
+var distributionTooltip = d3.select("body").append("div")
+  .attr("class", "tooltips")
+  .style("opacity", 0);
+
+var mapTooltip = d3.select("body").append("div")
   .attr("class", "tooltip")
   .style("opacity", 0);
 
@@ -19,37 +40,26 @@ promises.push(d3.json("data/indonesia.json"));
 
 Promise.all(promises)
   .then(values => {
-    ready(null, values[0], values[1]);
+    ready(values[0], values[1]);
   })
   .catch(error => console.error(error));
 
 var legendText = ["", "87.5", "90", "92.5", "95", "97.5"];
 var legendColors = ["#CCCCFF", "#66CCCC", "#6699CC", "#3366CC", "#333366"];
 
-
-function ready(error, data, us) {
-  // console.log(us.objects);
-  var counties = topojson.feature(us, us.objects.states_provinces);
-
-  // data.forEach(function (d) {
-  //   d.year = +d.year;
-  //   d.fips = +d.fips;
-  //   d.rate = +d.rate;
-  // });
-
-  var dataByCountyByYear = d3.nest()
+function ready(data, us) {
+  var provinces = topojson.feature(us, us.objects.states_provinces);
+  var dataByProvinces = d3.nest()
     .key(function (d) { return d.province; })
     .key(function (d) { return d.year; })
     .map(data);
+  var dataByYear = d3.nest()
+    .key(function (d) { return d.year; })
+    .map(data);
 
-  // console.log(dataByCountyByYear);
-  // console.log(counties.features);
-  counties.features.forEach(function (county) {
-    // console.log(county);
-    // console.log("$" + county.properties.name);
-    county.properties.years = dataByCountyByYear["$" + county.properties.name];
+  provinces.features.forEach(function (d) {
+    d.properties.years = dataByProvinces["$" + d.properties.name];
   });
-  // console.log(counties.features);
 
   var color = d3.scaleThreshold()
     .domain([87.5, 90, 92.5, 95, 97.5])
@@ -63,40 +73,36 @@ function ready(error, data, us) {
   var path = d3.geoPath()
     .projection(projection);
 
-  var countyShapes = svg.selectAll(".county")
-    .data(counties.features)
+  var provinceShapes = mapSvg.selectAll(".county")
+    .data(provinces.features)
     .enter()
     .append("path")
     .attr("class", "county")
     .attr("d", path);
 
-  countyShapes
+  provinceShapes
     .on("mouseover", function (d) {
-      tooltip.transition()
+      mapTooltip.transition()
         .duration(250)
         .style("opacity", 1);
-      tooltip.html(
-        "<p><strong>" + d.properties.years["$1996"][0].province + ", " + d.properties.years["$1996"][0].province + "</strong></p>" +
-        "<table><tbody><tr><td class='wide'>APM rate in 1996:</td><td>" + formatPercent((d.properties.years["$1996"][0].APM) / 100) + "</td></tr>" +
-        "<tr><td>APM rate in 1998:</td><td>" + formatPercent((d.properties.years["$1998"][0].APM) / 100) + "</td></tr>" +
-        "<tr><td>Change:</td><td>" + formatPercent((d.properties.years["$1998"][0].APM - d.properties.years["$1996"][0].APM) / 100) + "</td></tr></tbody></table>"
+      mapTooltip.html(
+        "<p><strong>" + d.properties.years["$1996"][0].province + "</strong></p>"
       )
         .style("left", (d3.event.pageX + 15) + "px")
         .style("top", (d3.event.pageY - 28) + "px");
     })
     .on("mouseout", function (d) {
-      tooltip.transition()
+      mapTooltip.transition()
         .duration(250)
         .style("opacity", 0);
     });
 
-  // console.log(us.objects.states_provinces);
-  svg.append("path")
+  mapSvg.append("path")
     .datum(topojson.feature(us, us.objects.states_provinces, function (a, b) { return a !== b; }))
     .attr("class", "states")
     .attr("d", path);
 
-  var legend = svg.append("g")
+  var legend = mapSvg.append("g")
     .attr("id", "legend");
 
   var legenditem = legend.selectAll(".legenditem")
@@ -120,15 +126,76 @@ function ready(error, data, us) {
     .style("text-anchor", "middle")
     .text(function (d, i) { return legendText[i]; });
 
+  // y-axis
+  distributionSvg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("class", "label")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Protein (g)");
+
+  // // draw dots
+  // var dots = newSvg.selectAll(".dot")
+  //   .data(dataByYear)
+  //   .enter().append("circle")
+  //   .attr("class", "dot")
+  //   .attr("r", 7)
+  //   .attr("cy", yMap)
+  //   .style("fill", function (d) { return color(d.APM); })
+  //   .on("mouseover", function (d) {
+  //     newTooltip.transition()
+  //       .duration(200)
+  //       .style("opacity", .9);
+  //     newTooltip.html(d.province + " " + d.APM)
+  //       .style("left", (d3.event.pageX + 5) + "px")
+  //       .style("top", (d3.event.pageY - 28) + "px");
+  //   })
+  //   .on("mouseout", function (d) {
+  //     newTooltip.transition()
+  //       .duration(500)
+  //       .style("opacity", 0);
+  //   });
+
   function update(year) {
     slider.property("value", year);
     d3.select(".year").text(year);
-    countyShapes.style("fill", function (d) {
+    provinceShapes.style("fill", function (d) {
       const years = d.properties.years;
       if (years) {
         return color(d.properties.years["$" + year][0].APM)
       }
     });
+
+    // draw dots
+    var circles = distributionSvg.selectAll(".dot")
+      .data(dataByYear["$" + year]);
+
+    circles.exit().remove();
+
+    circles.enter()
+      .append("circle")
+      .attr("class", "dot")
+      .attr("r", 7)
+      .attr("cy", yMap)
+      .style("fill", function (d) { return color(d.APM); })
+      .on("mouseover", function (d) {
+        distributionTooltip.transition()
+          .duration(200)
+          .style("opacity", .9);
+        distributionTooltip.html(d.province + "<br>" + d.APM + "%")
+          .style("left", (d3.event.pageX + 5) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
+      })
+      .on("mouseout", function (d) {
+        distributionTooltip.transition()
+          .duration(500)
+          .style("opacity", 0);
+      })
+      .merge(circles).transition();
   }
 
   var slider = d3.select(".slider")
