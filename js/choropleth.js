3c7d4bf8af014a9aaf3e8ae315fc76e51b5285ca
Jonathan Christopher <jonathan.christopher@outlook.com>
2019-05-09 15:26:51 +0700

    Adjusted choropleth map layout and year slider (level select and distribution not working yet)

--- js/choropleth.js
+++ js/choropleth.js
@@ -1,106 +1,65 @@
-var mapMargin = { top: 20, right: 20, bottom: 20, left: 20 };
-var width = 1000 - mapMargin.left - mapMargin.right,
-  height = 512 - mapMargin.top - mapMargin.bottom,
-  formatPercent = d3.format(".1%");
+function getDataPoint(data, year, province, level) {
+  if (data.byYearAndProvince['$' + year] && data.byYearAndProvince['$' + year]['$' + province]) {
+    return data.byYearAndProvince['$' + year]['$' + province][0][level];
+  }
+  return 0;
+}
 
-var distributionMargins = { top: 20, right: 20, bottom: 30, left: 40 },
-  widths = 90 - distributionMargins.left - distributionMargins.right,
-  heights = 600 - distributionMargins.top - distributionMargins.bottom;
+function getDataYears(data) {
+  console.log(data);
+  const years = Object.keys(data.byYear).map(x => +x.slice(1));
+  years.sort();
+  return years;
+}
 
-var yValue = function (d) { return d.APM },
-  yScale = d3.scaleLinear().range([height, 0])
-    .domain([0, 1]),
-  yMap = function (d) { return yScale(yValue(d)) },
-  yAxis = d3.axisLeft(yScale).tickSize(0);
+function drawChoropleth(topoJsonData, data, year, level) {
+  /*var legenditem = mapLegendContainer.selectAll(".legenditem")
+    .data(d3.range(6))
+    .enter()
+    .append("g")
+    .attr("class", "legenditem")
+    .attr("transform", function (d, i) { return "translate(" + i * 31 + ",0)"; });
 
-var mapSvg = d3.select("#map").append("svg")
-  .attr("width", width + mapMargin.left + mapMargin.right)
-  .attr("height", height + mapMargin.top + mapMargin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + mapMargin.left + "," + mapMargin.top + ")");
+  legenditem.append("rect")
+    .attr("x", width - 240)
+    .attr("y", -7)
+    .attr("width", 30)
+    .attr("height", 6)
+    .attr("class", "rect")
+    .style("fill", function (d, i) { return '#eee'; });
 
-var distributionSvg = d3.select("#distribution").append("svg")
-  .attr("width", widths + distributionMargins.left + distributionMargins.right)
-  .attr("height", heights + distributionMargins.top + distributionMargins.bottom)
-  .style("margin-left", distributionMargins.left)
-  .append("g")
-  .attr("transform", "translate(" + distributionMargins.left + "," + distributionMargins.top + ")");
+  legenditem.append("text")
+    .attr("x", width - 240)
+    .attr("y", 16)
+    .style("text-anchor", "middle")
+    .text(function (d, i) { return 'aaa'; });
+*/
 
-var distributionTooltip = d3.select("body").append("div")
-  .attr("class", "tooltip")
-  .style("opacity", 0);
-
-var mapTooltip = d3.select("body").append("div")
-  .attr("class", "tooltip")
-  .style("opacity", 0);
-
-var legendText = ["", "60", "65", "70", "75", "80", "85", "90", "95"];
-var legendColors = ["#bbdefb", "#90caf9", "#64b5f6", "#42a5f5", "#2196f3", "#1e88e5", "#1976d2", "#1565c0"];
-var color = d3.scaleThreshold()
-  .domain([60, 65, 70, 75, 80, 85, 90, 95])
-  .range(legendColors);
-
-var projection = d3.geoEquirectangular()
-  .scale(1050)
-  .rotate([-120, 0])
-  .translate([width / 2, height / 2]);
-
-var path = d3.geoPath()
-  .projection(projection);
-
-var promises = [];
-promises.push(d3.csv("data/apm.csv"));
-promises.push(d3.json("data/indonesia.json"));
-
-Promise.all(promises)
-  .then(values => {
-    var apmData = values[0];
-    var map = values[1];
-
-    var provinces = topojson.feature(map, map.objects.states_provinces);
-    var dataByProvinces = d3.nest()
-      .key(function (d) { return d.province; })
-      .key(function (d) { return d.year; })
-      .map(apmData);
-    var dataByYear = d3.nest()
-      .key(function (d) { return d.year; })
-      .map(apmData);
-
-    provinces.features.forEach(function (d) {
-      d.properties.years = dataByProvinces["$" + d.properties.name];
+  const years = getDataYears(data);
+  choroplethYearSlider
+    .min(years[0])
+    .max(years[years.length - 1])
+    .tickValues(years)
+    .marks(years)
+    .on('onchange', newYear => {
+      updateChoropleth(topoJsonData, data, newYear, level);
     });
 
-    mapSvg.append("path")
-      .datum(topojson.feature(map, map.objects.states_provinces, function (a, b) { return a !== b; }))
-      .attr("class", "states")
-      .attr("d", path);
+  const sliderContainer = choroplethSvg.append('g')
+    .attr('transform', 'translate(20, 432)');
 
-    var legend = mapSvg.append("g")
-      .attr("id", "legend");
+  sliderContainer.call(choroplethYearSlider);
+  sliderContainer.append('text')
+    .attr('fill', '#555')
+    .attr('transform', 'translate(-20, -20)')
+    .classed('slider-label', true);
 
-    var legenditem = legend.selectAll(".legenditem")
-      .data(d3.range(6))
-      .enter()
-      .append("g")
-      .attr("class", "legenditem")
-      .attr("transform", function (d, i) { return "translate(" + i * 31 + ",0)"; });
+  updateChoropleth(topoJsonData, data, year, level);
 
-    legenditem.append("rect")
-      .attr("x", width - 240)
-      .attr("y", -7)
-      .attr("width", 30)
-      .attr("height", 6)
-      .attr("class", "rect")
-      .style("fill", function (d, i) { return legendColors[i]; });
-
-    legenditem.append("text")
-      .attr("x", width - 240)
-      .attr("y", 16)
-      .style("text-anchor", "middle")
-      .text(function (d, i) { return legendText[i]; });
+/*
 
     // y-axis
-    distributionSvg.append("g")
+    distributionContainer.append("g")
       .attr("class", "y axis")
       .call(yAxis)
       .append("text")
@@ -132,10 +91,128 @@ Promise.all(promises)
       updateMapAndDistribution(year, educationLevel, provinces, dataByYear);
     });
     updateMapAndDistribution(2003, "SD", provinces, dataByYear);
+*/
+  
+}
+
+function updateChoropleth(topoJsonData, data, year, level) {
+  const geoJsonData = topojson.feature(topoJsonData, topoJsonData.objects.states_provinces);
+
+  const mapPaths = mapContainer.selectAll('path')
+    .data(geoJsonData.features);
+
+  mapPaths.exit().remove();
+  const mapPathsEnter = mapPaths.enter()
+    .append('path')
+      .attr('class', 'province')
+    .on('mouseenter', d => {
+      d3.select(d3.event.target).attr('stroke', '#222');
+      mapTooltip.transition().style('opacity', 0.9);
+      mapTooltip.html(d.properties.province + '<br />' + getDataPoint(data, year, d.properties.province, level) + '%');
+    })
+    .on('mousemove', d => {
+      mapTooltip
+        .style('left', (d3.event.pageX) + 'px')
+        .style('top', (d3.event.pageY) + 'px');
+    })
+    .on('mouseleave', d => {
+      d3.select(d3.event.target).attr('stroke', '');
+      mapTooltip.transition().style('opacity', 0);
+    });
+
+  mapPathsEnter.merge(mapPaths)
+    .transition()
+      .attr('d', geoPath)
+      .style('fill', d => choroplethColorScale(getDataPoint(data, year, d.properties.province, level)));
+
+  choroplethYearSlider.silentValue(year);
+  choroplethSvg.select('.slider-label').text('Tahun ' + year);
+}
+
+// Setup projection
+const geoPath = d3.geoPath().projection(
+  d3.geoEquirectangular()
+  .scale(1000)
+  .translate([-1650, 110])
+);
+
+// Setup color scale
+//var legendText = ["", "60", "65", "70", "75", "80", "85", "90", "95"];
+const legendColors = ["#bbdefb", "#90caf9", "#64b5f6", "#42a5f5", "#2196f3", "#1e88e5", "#1976d2", "#1565c0"];
+const choroplethColorScale = d3.scaleThreshold()
+  .domain([60, 65, 70, 75, 80, 85, 90, 95])
+  .range(legendColors);
+
+// Setup container SVG
+const choroplethSvg = d3.select('#choropleth')
+  .attr('viewBox', '0 0 1200 480');
+
+const mapContainer = choroplethSvg.append('g')
+  .classed('map', true);
+
+const mapLegendContainer = choroplethSvg.append('g')
+  .classed('map-legend', true)
+  .attr('transform', 'translate(480, 0)')
+  .attr('fill', '#eee');
+
+const distributionContainer = choroplethSvg.append('g')
+  .attr('transform', 'translate(720, 0)')
+  .attr('fill', '#eee');
+
+// Setup tooltips
+const mapTooltip = d3.select('body').append('div')
+  .attr('id', 'map-tooltip')
+  .attr('class', 'tooltip')
+  .style("opacity", 0);
+
+const distributionTooltip = d3.select("body").append("div")
+  .attr('id', 'distribution-tooltip')
+  .attr('class', 'tooltip')
+  .style("opacity", 0);
+
+const choroplethYearSlider = d3
+  .sliderHorizontal()
+  .width(774)
+  .tickFormat(d3.format('d'))
+  .displayValue(false);
+
+/*
+var yValue = function (d) { return d.APM },
+  yScale = d3.scaleLinear().range([height, 0])
+    .domain([0, 1]),
+  yMap = function (d) { return yScale(yValue(d)) },
+  yAxis = d3.axisLeft(yScale).tickSize(0);
+
+
+*/
+
+Promise.all([
+  d3.csv('data/choropleth.csv'),
+  d3.json('data/indonesia.json')
+])
+  .then(([data, topoJsonData]) => {
+    const provinces = topojson.feature(topoJsonData, topoJsonData.objects.states_provinces);
+    const indexedData = {
+      byYear: d3.nest()
+        .key(d => d.year)
+        .map(data),
+      byYearAndProvince: d3.nest()
+        .key(d => d.year)
+        .key(d => d.province)
+        .map(data)
+    };
+
+    provinces.features.forEach(d => {
+      d.properties.province = d.properties.name;
+    });
+
+    const years = getDataYears(indexedData);
+    const initialYear = years[years.length - 1];
+    drawChoropleth(topoJsonData, indexedData, initialYear, 'sma');
   })
-  .catch(error => console.error(error));
+  .catch(err => console.error(err));
 
-
+/*
 function updateMapAndDistribution(year, educationLevel, provinces, dataByYear) {
   d3.select(".year").text(year);
 
@@ -205,3 +282,4 @@ function updateMapAndDistribution(year, educationLevel, provinces, dataByYear) {
 }
 
 d3.select(self.frameElement).style("height", "685px");
+*/
