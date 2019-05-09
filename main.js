396cdff68c262181013d2f313dc8fa8f79b19e94
Jonathan Christopher <jonathan.christopher@outlook.com>
2019-05-09 11:42:06 +0700

    Added labels and legend to the Sankey diagram

--- main.js
+++ main.js
@@ -1,42 +1,60 @@
+const categoryColors = {
+  'lanjut': '#bedcf4',
+  'tidak-lanjut': '#e5d3bd',
+  'do': '#ffdf9e'
+};
+
+const levels = {
+  'sd': 'SD',
+  'smp': 'SMP',
+  'sma': 'SMA/SMK'
+};
+
+const nextLevelDescription = {
+  'sd': 'SMP',
+  'smp': 'SMA/SMK',
+  'sma': 'perguruan tinggi'
+};
+
+const categories = {
+  'lanjut': 'Lanjut ke jenjang berikutnya',
+  'tidak-lanjut': 'Tidak lanjut setelah lulus',
+  'do': 'Drop out'
+};
+
+const getLinkDescription = function (category, sourceLevel) {
+  if (category === 'lanjut') {
+    return 'Lanjut ke ' + nextLevelDescription[sourceLevel];
+  } else if (category === 'tidak-lanjut') {
+    return 'Tidak lanjut sekolah setelah lulus ' + levels[sourceLevel];
+  } else if (category === 'do') {
+    return 'Drop out dari ' + levels[sourceLevel];
+  } else {
+    return '';
+  }
+}
+
+const buckets = {
+  'lanjut-pt': 'Lanjut ke perguruan tinggi',
+  'tamat-sma': 'Tamat SMA/SMK',
+  'tamat-smp': 'Tamat SMP',
+  'tamat-sd': 'Tamat SD',
+  'tidak-lulus-sd': 'Tidak lulus SD'
+};
+
+const targetMap = {
+  'sma:lanjut': 'lanjut-pt',
+  'sma:tidak-lanjut': 'tamat-sma',
+  'sma:do': 'tamat-smp',
+  'smp:lanjut': 'sma',
+  'smp:tidak-lanjut': 'tamat-smp',
+  'smp:do': 'tamat-sd',
+  'sd:lanjut': 'smp',
+  'sd:tidak-lanjut': 'tamat-sd',
+  'sd:do': 'tidak-lulus-sd'
+};
+
 function transformSankeyData(rawData, year) {
-
-  const levels = {
-    'sd': 'SD',
-    'smp': 'SMP',
-    'sma': 'SMA/SMK'
-  };
-
-  const categories = {
-    'lanjut': 'Melanjutkan ke jenjang berikutnya',
-    'tidak-lanjut': 'Tidak melanjutkan setelah lulus',
-    'do': 'Drop out'
-  };
-
-  const categoryColors = {
-    'lanjut': '#bedcf4',
-    'tidak-lanjut': '#e5d3bd',
-    'do': '#ffdf9e'
-  };
-
-  const buckets = {
-    'lanjut-pt': 'Melanjutkan ke perguruan tinggi',
-    'tamat-sma': 'Tamat SMA/SMK',
-    'tamat-smp': 'Tamat SMP',
-    'tamat-sd': 'Tamat SD',
-    'tidak-lulus-sd': 'Tidak lulus SD'
-  };
-
-  const targetMap = {
-    'sma:lanjut': 'lanjut-pt',
-    'sma:tidak-lanjut': 'tamat-sma',
-    'sma:do': 'tamat-smp',
-    'smp:lanjut': 'sma',
-    'smp:tidak-lanjut': 'tamat-smp',
-    'smp:do': 'tamat-sd',
-    'sd:lanjut': 'smp',
-    'sd:tidak-lanjut': 'tamat-sd',
-    'sd:do': 'tidak-lulus-sd'
-  };
 
   const levelKeys = Object.keys(levels);
   const categoryKeys = Object.keys(categories);
@@ -59,7 +77,7 @@ function transformSankeyData(rawData, year) {
       links.push({
         source: levelKeys[i],
         target: targetMap[combinedKey],
-        title: categories[categoryKeys[j]],
+        description: getLinkDescription(categoryKeys[j], levelKeys[i]),
         color: categoryColors[categoryKeys[j]],
         value: +currentYearData[combinedKey] // Convert CSV values (string) to number
       });
@@ -70,13 +88,15 @@ function transformSankeyData(rawData, year) {
   for (let i = 0; i < levelKeys.length; i++) {
     nodes.push({
       id: levelKeys[i],
-      title: levels[levelKeys[i]]
+      title: levels[levelKeys[i]],
+      isLevel: true
     })
   }
   for (let i = 0; i < bucketKeys.length; i++) {
     nodes.push({
       id: bucketKeys[i],
-      title: buckets[bucketKeys[i]]
+      title: buckets[bucketKeys[i]],
+      isLevel: false
     })
   }
 
@@ -90,18 +110,32 @@ function drawSankey(rawData, year) {
   sankeySvg.append('g')
     .classed('nodes', true);
 
+  sankeySvg.append('g')
+    .classed('node-labels', true);
+
+
   const years = rawData.map(row => row.year);
   yearSlider
     .min(years[0])
     .max(years[years.length - 1])
     .tickValues(years)
+    .marks(years)
     .on('onchange', newYear => {
       updateSankey(rawData, newYear);
     });
 
   sankeySvg.append('g')
-    .attr('transform', 'translate(20, 460)')
-    .call(yearSlider);
+    .classed('legend', true)
+    .attr('transform', 'translate(0, 300)');
+
+  const sliderContainer = sankeySvg.append('g')
+    .attr('transform', 'translate(20, 432)');
+
+  sliderContainer.call(yearSlider);
+  sliderContainer.append('text')
+    .attr('fill', '#555')
+    .attr('transform', 'translate(-20, -20)')
+    .classed('slider-label', true)
 
   updateSankey(rawData, year);
 }
@@ -121,15 +155,33 @@ function updateSankey(rawData, year) {
 
   links.exit().remove();
 
-  links.enter()
+  const linksEnter = links.enter()
     .append('path')
       .classed('link', true)
       .attr('fill', 'none')
-    .merge(links)
-      .transition()
-        .attr('d', d3.sankeyLinkHorizontal())
-        .attr('stroke', d => d.color)
-        .attr('stroke-width', d => d.width);
+      .attr('pointer-events', 'visibleStroke')
+      .attr('opacity', 1)
+      .on('mouseenter', d => {
+        sankeySvg.selectAll('.link').transition().attr('opacity', 0.5);
+        d3.select(d3.event.target).transition().attr('opacity', 1);
+        sankeyTooltip.transition().style('opacity', 0.9);
+        sankeyTooltip.html(d.description + '<br />' + d.value + '% siswa');
+      })
+      .on('mousemove', d => {
+        sankeyTooltip
+          .style('left', (d3.event.pageX) + 'px')
+          .style('top', (d3.event.pageY) + 'px');
+      })
+      .on('mouseleave', d => {
+        sankeySvg.selectAll('.link').transition().delay(200).attr('opacity', 1);
+        sankeyTooltip.transition().style('opacity', 0);
+      });
+
+  const linksUpdate = linksEnter.merge(links);
+  linksUpdate.transition()
+    .attr('d', d3.sankeyLinkHorizontal())
+    .attr('stroke', d => d.color)
+    .attr('stroke-width', d => d.width);
 
   // Draw nodes
   const nodes = sankeySvg.select('.nodes')
@@ -149,26 +201,85 @@ function updateSankey(rawData, year) {
         .attr('width', d => d.x1 - d.x0)
         .attr('height', d => d.y1 - d.y0);
 
+  // Draw node labels
+  const nodeLabels = sankeySvg.select('.node-labels')
+      .selectAll('text')
+      .data(sankeyGraph.nodes);
+
+  nodeLabels.exit().remove();
+
+  const updatedLabels = nodeLabels.enter()
+      .append('text')
+        .classed('node-label', true)
+      .merge(nodeLabels)
+        .html(d => d.isLevel
+          ? ('<tspan class="bold">' + d.title + '</tspan> <tspan class="">(' + d.value + '%)</tspan>')
+          : ('<tspan x="0" dy="1.2em" class="">' + d.title + '</tspan><tspan x="0" dy="1.2em" class="bold">' + d.value + '%</tspan>'))
+
+  updatedLabels.transition()
+    .attr('transform', d => 'translate(' + (d.isLevel ? d.x0 : d.x1 + 6) + ', 0)')
+    .attr('y', d => d.isLevel ? d.y0 - 10 : (d.y0 + d.y1) / 2 - 20);
+
+  updatedLabels.selectAll('.bold')
+    .style('font-weight', 700);
+
+  const legendKeys = ['do', 'tidak-lanjut', 'lanjut'];
+
+  const legendRects = sankeySvg.select('.legend')
+    .selectAll('rect')
+    .data(legendKeys);
+
+  legendRects.exit().remove();
+  legendRects.enter()
+    .append('rect')
+      .attr('x', 0)
+      .attr('width', 14)
+      .attr('height', 14)
+    .merge(legendRects)
+      .attr('y', (d, i) => i * 22)
+      .attr('fill', d => categoryColors[d]);
+
+  const legendLabels = sankeySvg.select('.legend')
+    .selectAll('text')
+    .data(legendKeys);
+
+  legendLabels.exit().remove();
+  legendLabels.enter()
+    .append('text')
+      .classed('legend-label', true)
+      .attr('x', 22)
+    .merge(legendLabels)
+      .attr('y', (d, i) => i * 22 + 12)
+      .html(d => categories[d]);
+
   yearSlider.silentValue(year);
+  sankeySvg.select('.slider-label').text('Angkatan ' + year)
+    .append('title')
+    .text('Siswa angkatan ' + year + ' adalah siswa yang masuk kelas 1 SD pada tahun ' + (year - 12))
 }
 
 // Setup Sankey generator
 const sankey = d3.sankey()
-  .extent([[0, 24], [600, 400]])
+  .extent([[0, 24], [480, 320]])
   .nodeId(d => d.id)
   .nodeAlign(d3.sankeyJustify)
   .nodeSort(null) // Follow input order
-  .nodePadding(20)
-  .nodeWidth(6);
+  .nodePadding(16)
+  .nodeWidth(5);
 
 // Setup container SVG
 const sankeySvg = d3.select('#sankey-diagram')
-  .attr('viewBox', '0 0 750 500');
+  .attr('viewBox', '0 0 720 480');
+
+// Setup tooltip
+var sankeyTooltip = d3.select('body').append('div')
+  .attr('id', 'sankey-tooltip')
+  .attr('class', 'tooltip')
+  .style("opacity", 0);
 
 const yearSlider = d3
   .sliderHorizontal()
-  .step(1)
-  .width(400)
+  .width(444)
   .tickFormat(d3.format('d'))
   .displayValue(false);
 
